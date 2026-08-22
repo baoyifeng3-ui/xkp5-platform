62672ddb9119972ec029968b4e2b5f57461f7c89
baoyifeng3 <baoyifeng3@gmail.com>
2026-08-23 07:43:19 +0800

    feat: allow administrators to create training environments

--- java/match-mgr/src/main/java/com/match/environment/web/AdminTrainingEnvironmentController.java
+++ java/match-mgr/src/main/java/com/match/environment/web/AdminTrainingEnvironmentController.java
@@ -2,11 +2,14 @@ package com.match.environment.web;
 
 import com.match.entity.User;
 import com.match.environment.service.EnvironmentOperationService;
+import com.match.environment.service.TrainingEnvironmentService;
+import com.match.environment.model.CreateTrainingEnvironmentRequest;
 import com.match.security.RoleGuard;
 import com.match.util.result.Response;
 import com.match.util.result.ResponseResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,17 +19,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminTrainingEnvironmentController {
     private final RoleGuard roleGuard;
     private final EnvironmentOperationService operationService;
+    private final TrainingEnvironmentService environmentService;
 
     public AdminTrainingEnvironmentController(RoleGuard roleGuard,
-                                              EnvironmentOperationService operationService) {
+                                              EnvironmentOperationService operationService,
+                                              TrainingEnvironmentService environmentService) {
         this.roleGuard = roleGuard;
         this.operationService = operationService;
+        this.environmentService = environmentService;
     }
 
     @GetMapping
     public ResponseResult<Object> list() {
         roleGuard.requireAnyAdmin();
         return Response.makeOKRsp(operationService.listAll());
+    }
+
+    @PostMapping
+    public ResponseResult<Object> create(@RequestBody CreateTrainingEnvironmentRequest request) {
+        User actor = roleGuard.requireAnyAdmin();
+        return Response.makeOKRsp(environmentService.create(request, actor.getUserId(),
+                roleGuard.roleOf(actor).name()));
     }
 
     @PostMapping("/{environmentId}/start")
