51e683e4cc834068395292e5fa499f7c29e44577
baoyifeng3 <baoyifeng3@gmail.com>
2026-08-24 02:13:50 +0800

    fix: import course cover path binding

--- java/match-mgr/src/main/java/com/match/course/web/UserCourseController.java
+++ java/match-mgr/src/main/java/com/match/course/web/UserCourseController.java
@@ -6,6 +6,7 @@ import com.match.security.RoleGuard;
 import com.match.util.result.Response;
 import com.match.util.result.ResponseResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
